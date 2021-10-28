64203f9dd751cbb958210aecf1dfc3f409c6e13f
Pavithra2304 <92969170+Pavithra2304@users.noreply.github.com>
2021-10-28 16:52:48 +0530

    Add files via upload

---
+++ String_2_/String_2_/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_2_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			StringBuilder details;
+			details = new StringBuilder();
+			Console.WriteLine("Please Enter your Name");
+			details.Insert(0, Console.ReadLine());
+			Console.WriteLine("Please Enter your age");
+			details.Insert(1, Console.ReadLine());
+			Console.WriteLine("Please Enter your Qualification");
+			details.Insert(2, Console.ReadLine());
+			Console.WriteLine("Please Enter your Experience");
+			details.Insert(3, Console.ReadLine());
+			Console.WriteLine(details);
+			Console.Read();
+		}
+	}
+}
